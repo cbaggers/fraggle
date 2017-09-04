9cad9036b515d7dcf8385004ab7cd92926dae989
cbaggers <chris.bagley@gmail.com>
2017-09-04 19:39:21 +0200

    wip

--- main.lisp
+++ main.lisp
@@ -6,18 +6,19 @@
           (* (+ pos (v2! 1f0)) 0.5)))
 
 
+;; (blend (vec4 (11to01 (perlin-noise (v! (* uv 8) now))))
+;;        (graph (lambda ((x :float))
+;;                 (perlin-noise (v! (* 8 x) 0 now)))
+;;               uv
+;;               (v! -1 1 -1 1)))
+
 (defun-g fraggle-quad-frag ((uv :vec2)
                             &uniform (now :float)
                             (mouse :vec2)
                             (mouse-norm :vec2)
                             (mouse-buttons :vec2)
                             (screen-res :vec2))
-  (v! (x mouse-norm) (x mouse-buttons) (y mouse-norm) 1)
-  (blend (vec4 (11to01 (perlin-noise (v! (* uv 8) now))))
-         (graph (lambda ((x :float))
-                  (perlin-noise (v! (* 8 x) 0 now)))
-                uv
-                (v! -1 1 -1 1))))
+  (v! (x mouse-norm) (x mouse-buttons) (y mouse-norm) 1))
 
 
 (defpipeline-g draw-fraggle ()
