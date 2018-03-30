a0debbd8b171d0074eba10a42b3d1c8894e41d35
cbaggers <chris.bagley@gmail.com>
2018-03-30 15:24:18 +0200

    yay sdfs

--- main.lisp
+++ main.lisp
@@ -12,6 +12,41 @@
              (sin (* 0.1 i)))
           (y disp))))
 
+(define-pgraph boop (:height-color)
+    (coord (now :float))
+  (let* ((now (* now 0.1))
+         (pos (- coord (vec2 100)))
+         ;;
+         (dist (merge-smooth
+                (merge-smooth (triangle pos
+                                        50)
+                              (circle (- pos (* (vec2 (sin (* now 0.18))
+                                                      (* (cos (* 1 now)) 1.4))
+                                                60))
+                                      10)
+                              10f0)
+                (circle (- pos (* (vec2 (sin now)
+                                        (cos now))
+                                  50))
+                        20)
+                10))
+         (dist-col (vec4 dist))
+         ;;
+         (masked (mask-fill dist))
+         (masked-col (mix (vec4 0)
+                          (vec4 0.7)
+                          masked))
+         ;;
+         (final-val masked)
+         (final-col masked-col))
+    ;;
+    (values dist
+            masked-col)))
+
+(define-pgraph plain (:height)
+    (coord (at :float))
+  at)
+
 ;;------------------------------------------------------------
 
 (defparameter *rot* (q:identity))
@@ -22,9 +57,11 @@
   (let* ((mmove (mouse-move (mouse)))
          (xmove (* (x mmove) 0.01))
          (ymove (* (y mmove) 0.01))
-         (yax (q:from-axis-angle (v! 0 1 0) xmove))
-         (xax (q:from-axis-angle (v! 1 0 0) ymove))
-         (rot (q:* *rot* (q:* yax xax)))
+         (yax (q:from-axis-angle (v! 0 1 0) (- xmove)))
+         (xax (q:from-axis-angle (v! 1 0 0) (- ymove)))
+         (rot (if (mouse-down-p mouse.left)
+                  (q:normalize (q:* *rot* (q:normalize (q:* xax yax))))
+                  *rot*))
          (dir (q:to-direction *rot*)))
     (when (key-down-p key.w)
       (v3:incf *pos* (v3:*s *dir* 1f0)))
@@ -37,13 +74,34 @@
 (defun step-fraggle ()
   (update-controls)
   (nineveh:as-frame
-    (woop *pos*
+    ;; (woop *pos*
+    ;;       *dir*
+    ;;       :min 0
+    ;;       :max 200000
+    ;;       :point-size 1.3
+    ;;       :point-color (v! 0.1 0.1 0.19 1)
+    ;;       :now (float (now) 0f0))
+    (boop *pos*
           *dir*
-          :min 0
-          :max 200000
-          :point-size 1.3
+          :x-min 0
+          :x-max 200
+          :y-min 0
+          :y-max 200
+          :by 1
           :point-color (v! 0.1 0.1 0.19 1)
-          :now (float (now) 0f0))))
+          :now (* 0.01 (now))
+          :spacing 1.4)
+
+    (plain *pos*
+          *dir*
+          :x-min 0
+          :x-max 200
+          :y-min 0
+          :y-max 200
+          :by 1
+          :point-color (v! 0.2 0.01 0.01 1)
+          :at 0.0
+          :spacing 1.4)))
 
 
 (def-simple-main-loop fraggle
