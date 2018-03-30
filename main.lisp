41e020d162301e71c9bf8286c3673db97bf41cc7
cbaggers <chris.bagley@gmail.com>
2018-03-30 12:07:07 +0200

    just using this as a place to test nineveh as I develop these graphs

--- main.lisp
+++ main.lisp
@@ -2,67 +2,48 @@
 
 ;;------------------------------------------------------------
 
-(defun-g fraggle-quad-vert ((pos :vec2))
-  (values (v! pos 0 1)
-          (* (+ pos (v2! 1f0)) 0.5)))
-
-(defun-g fraggle-quad-frag ((uv :vec2)
-                            &uniform (now :float)
-                            (mouse :vec2)
-                            (mouse-norm :vec2)
-                            (mouse-buttons :vec2)
-                            (screen-res :vec2))
-  (let ((half-res (/ screen-res 2)))
-    (flet ((func ((p :vec2))
-             (merge-simple
-              (merge-simple
-               (circle (translate p (v! 200 150)) 80)
-               (triangle (rotate-ccw p now)
-                         10))
-              (semicircle (rotate-ccw p (* 0.5 now))
-                          80 120 20))))
-      (let* ((mouse-pos (- mouse half-res))
-             (p (- (s~ gl-frag-coord :xy)
-                   half-res))
-             (p (* p 1))
-             (dist (func p))
-             (light-col (apply-luminance
-                         #'(rgb->luma-bt601 :vec4)
-                         0.6
-                         (vec4 0.75 1.0 0.5 1.0)))
-             (light (point-light #'func
-                                 p
-                                 mouse-pos
-                                 light-col
-                                 250.0
-                                 16.0)))
-        (mix
-         (mix
-          (mix
-           (+ (v! 0.03 0.03 0.04 1)
-              light)
-           (v! 0.7 0 0 1)
-           (mask-border-outer dist 3))
-          (v! 0 0.1 0.8 1)
-          (mask-border-inner dist 3))
-         (v! 0 0 0 0)
-         (mask-fill dist))))))
-
-
-(defpipeline-g draw-fraggle ()
-  :vertex (fraggle-quad-vert :vec2)
-  :fragment (fraggle-quad-frag :vec2))
+(define-pgraph woop (:range)
+    (i (now :float))
+  (let* ((ang (* i 0.01))
+         (disp (* (vec2 (sin ang) (cos ang))
+                  (* i 0.002))))
+    (vec3 (x disp)
+          (* (+ 40 (* 2 (cos (* (+ (* 0.1 i) now) 0.001))))
+             (sin (* 0.1 i)))
+          (y disp))))
 
 ;;------------------------------------------------------------
 
+(defparameter *rot* (q:identity))
+(defparameter *dir* (v! 0 0 -1))
+(defparameter *pos* (v! -10 80 200))
+
+(defun update-controls ()
+  (let* ((mmove (mouse-move (mouse)))
+         (xmove (* (x mmove) 0.01))
+         (ymove (* (y mmove) 0.01))
+         (yax (q:from-axis-angle (v! 0 1 0) xmove))
+         (xax (q:from-axis-angle (v! 1 0 0) ymove))
+         (rot (q:* *rot* (q:* yax xax)))
+         (dir (q:to-direction *rot*)))
+    (when (key-down-p key.w)
+      (v3:incf *pos* (v3:*s *dir* 1f0)))
+    (when (key-down-p key.s)
+      (v3:incf *pos* (v3:*s *dir* -1f0)))
+    (setf *rot* rot)
+    (setf *dir* dir))
+  (decay-events))
+
 (defun step-fraggle ()
-  (as-frame
-    (map-g #'draw-fraggle (get-quad-stream-v2)
-           :now (* (now) 0.001)
-           :mouse (get-mouse)
-           :mouse-norm (get-mouse-norm)
-           :mouse-buttons (get-mouse-buttons)
-           :screen-res (viewport-resolution (current-viewport)))))
+  (update-controls)
+  (nineveh:as-frame
+    (woop *pos*
+          *dir*
+          :min 0
+          :max 200000
+          :point-size 1.3
+          :point-color (v! 0.1 0.1 0.19 1)
+          :now (float (now) 0f0))))
 
 
 (def-simple-main-loop fraggle
